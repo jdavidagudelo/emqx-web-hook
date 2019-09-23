ccf3ad5c3f38ab4d5b18f9b26ac91ad007db0830
jdavidagudelo <jdaaa2009@gmail.com>
2019-09-23 10:44:55 -0500

    Fixing some issues.

--- src/emqx_web_hook.erl
+++ src/emqx_web_hook.erl
@@ -231,8 +231,11 @@ on_message_publish(Message = #message{topic = Topic, flags = #{retain := Retain}
 %%--------------------------------------------------------------------
 %% Message deliver
 %%--------------------------------------------------------------------
-
+on_message_deliver(ClientInfo, Message, Filter) ->
+  ?LOG(error, "Client disconnected, cannot encode reason: ~p ~p ~p", [ClientInfo, Message, Filter]),
+  ok;
 on_message_deliver(#{client_id := ClientId, username := Username}, Message = #message{topic = Topic, flags = #{retain := Retain}}, {Filter}) ->
+
   with_filter(
     fun() ->
       emqx_metrics:inc('web_hook.message_deliver'),
