584ca92bed9e9c8726a4cff2f94cdcab4b48fa08
jdavidagudelo <jdaaa2009@gmail.com>
2020-09-14 14:42:08 -0500

    Fixing some issues.

--- src/emqx_web_hook.erl
+++ src/emqx_web_hook.erl
@@ -83,7 +83,7 @@ decode_clientid(ClientId) ->
     IsNotBinaryClientId = not is_binary(ClientId),
     if IsValidClientId or IsNotBinaryClientId ->
         ClientId;
-        true -> ClientId
+        true -> encode_payload(binary:bin_to_list(ClientId), base64)
     end.
 
 %%--------------------------------------------------------------------
